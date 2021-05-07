f39fa1aea28fca4d2bd8cdab01838d5cbe235fa8
osakoh <macmive@gmail.com>
2021-05-07 20:55:31 +0100

    implemented route to add new Post

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,11 +1,11 @@
 // comments
 
 const express = require("express");
-// express router
-const router = express.Router();
+const router = express.Router(); // express router
 const mongoose = require("mongoose");
 const passport = require("passport");
-const Post = require("../../models/Post");
+const Post = require("../../models/Post"); // Post model
+const validatePostInput = require("../../validation/Post");
 
 // @route       GET api/posts
 // @description Create/Add post
@@ -14,6 +14,14 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // check validation
+    if (!isValid) {
+      // send 400 status
+      return res.status(400).json(errors);
+    }
+
     // newPost object to hold fields retrieved from the body of the request
     const newPost = new Post({
       text: req.body.text,
