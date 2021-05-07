6c56b9786de506d032f2abe906a1fecf3d61cc1f
osakoh <macmive@gmail.com>
2021-05-08 00:06:16 +0100

    added route to add comment to post

--- routes/api/post.js
+++ routes/api/post.js
@@ -185,4 +185,42 @@ router.post(
   }
 );
 
+// @route         POST api/posts/comment/:id
+// @description   Add comment to post based on the post id
+// @access        Private
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // check validation
+    if (!isValid) {
+      // send 400 status
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+      .then((post) => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id,
+        };
+
+        // add to comments array
+        post.comments.unshift(newComment);
+        // save to DB
+        post
+          .save()
+          .then((post) => res.json(post))
+          .catch((err) =>
+            res.status(404).json({ postnotfound: "No post found" })
+          );
+      })
+      .catch((err) => res.status(404).json({ postnotfound: "Post on found" }));
+  }
+);
+
 module.exports = router;
