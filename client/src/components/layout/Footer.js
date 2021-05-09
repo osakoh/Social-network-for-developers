ca4aa14e42e87a4f216a16b5bc18eba7a1babb6e
osakoh <macmive@gmail.com>
2021-05-09 21:02:30 +0100

    created footer, landing & navbar components

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="navBg text-white mt-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} DevNet
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import logo from "../../../src/logo.png";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark mb-4 navBg">
+        <div className="container">
+          <a className="navbar-brand" href="landing.html">
+            <img src={logo} alt={"logo"} className="navPhoto" />
+          </a>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="profiles.html">
+                  {" "}
+                  Developers
+                </a>
+              </li>
+            </ul>
+
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="register.html">
+                  Sign Up
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="login.html">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
