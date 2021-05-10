959f291da2f644d717ac92164b77f20b9acdaeb2
osakoh <macmive@gmail.com>
2021-05-10 10:22:02 +0100

    constructor for component state in Register JS added

--- client/src/components/layout/authenticate/Register.js
+++ client/src/components/layout/authenticate/Register.js
@@ -1,10 +1,76 @@
 import React, { Component } from "react";
 
 class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      password2: "",
+      errors: {},
+    };
+  }
+
   render() {
     return (
-      <div>
-        <h1>Register</h1>
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">
+                Create your DevConnector account
+              </p>
+              <form action="create-profile.html">
+                <div className="form-group mb-2">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Name"
+                    name="name"
+                    required
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                  />
+                  <small className="form-text text-muted">
+                    This site uses Gravatar so if you want a profile image, use
+                    a Gravatar email
+                  </small>
+                </div>
+                <div className="form-group mb-2">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Confirm Password"
+                    name="password2"
+                  />
+                </div>
+                <div className="d-grid">
+                  <input
+                    type="submit"
+                    className="btn btn-dark btn-block btn-lg mt-2 "
+                    value="Sign up"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
