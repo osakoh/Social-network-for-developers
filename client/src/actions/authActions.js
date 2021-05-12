65d2a1eb564f4a8de87aea80f69894808b7bd0fd
osakoh <macmive@gmail.com>
2021-05-12 16:15:33 +0100

    added redirect to login page on successful registration

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 import { GET_ERRORS } from "./types";
 
-// register user
-export const registerUser = (userData) => (dispatch) => {
+/**
+ * register user
+ * userData param: reps newUser object from the form in Register JS
+ * history param: reps this.props.history from Register JS to allow for redirection in actions
+ *
+ * dispatch: used to dispatch a type to a reducer (authReducer)
+ */
+export const registerUser = (userData, history) => (dispatch) => {
   axios
     .post("/api/users/register", userData)
     // redirect to login page
-    .then((res) => console.log(res.data))
+    .then((res) => history.push("/login"))
     .catch((err) => dispatch({ type: GET_ERRORS, payload: err.response.data }));
 };
 
