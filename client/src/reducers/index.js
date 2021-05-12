eb303c69248351b1a2a0c9217a91733bcb0adf70
osakoh <macmive@gmail.com>
2021-05-12 15:50:05 +0100

    added errorReducer to the combineReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
-// this is the root reducer
+// this is the root reducer: takes in all the reducers
 
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import errorReducer from "./errorReducer";
 
 export default combineReducers({
   auth: authReducer,
+  errors: errorReducer,
 });
