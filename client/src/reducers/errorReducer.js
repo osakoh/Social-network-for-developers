046329dc712beba858ce82a88ec3d01bce913dfc
osakoh <macmive@gmail.com>
2021-05-12 14:53:26 +0100

    created errorReducer js

---
+++ client/src/reducers/errorReducer.js
@@ -0,0 +1,20 @@
+import { GET_ERRORS } from "../actions/types";
+
+const initialState = {};
+
+/**
+ * reducer: is a function that contains a state and an action. It deceides what the state would be after
+ * each action. So, if a button is clicked in one of the components, an action is called in
+ * which then dispatches a type and/or a payload(data) to the reducer.
+ * The reducer then sends it down to any component that requires it.
+ */
+const errorReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ERRORS:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default errorReducer;
