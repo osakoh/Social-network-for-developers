416cb3a665c7731e8e2df05ed2a9675716f88486
osakoh <macmive@gmail.com>
2021-05-14 19:57:34 +0100

    setup profileReducer

---
+++ client/src/reducers/profileReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  profile: null,
+  profiles: null,
+  loading: false,
+};
+
+const profileReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default profileReducer;
