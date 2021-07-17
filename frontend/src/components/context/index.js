08e91f9f770cd2a25604c134e34f02c7e70e9346
osakoh <macmive@gmail.com>
2021-07-18 00:06:39 +0100

    created combined context

---
+++ frontend/src/components/context/index.js
@@ -0,0 +1,11 @@
+// import { combineContexts } from "react-combine-contexts";
+// import authContext from "./Auth/authContext";
+// import profileContext from "./Profile/profileContext";
+// import postContext from "./post/postContext";
+// // import alertContext from './alert/alertContext'
+
+// const CombinedContext = combineContexts({
+//   auth: authContext,
+//   profile: profileContext,
+//   post: postContext,
+// });
